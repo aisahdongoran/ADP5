c59ed9ab03af101ae616674f7c73be1d3778f7d2
Nur Aisah Dongoran <42975774+aisahdongoran@users.noreply.github.com>
2019-10-08 13:46:07 +0700

    Add files via upload

---
+++ Kalkulator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Kalkulator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int c;
+            int a = int.Parse(args[0]);
+            int b = int.Parse(args[2]);
+
+            if (args[1] == "+")
+            {
+                c = a + b;
+                Console.Write("={0}", c);
+            }
+            else if (args[1] == "-")
+            {
+                c = a - b;
+                Console.Write("={0}", c);
+            }
+
+            else if (args[1] == "/")
+            {
+                c = a / b;
+                Console.Write("={0}", c);
+            }
+            else if (args[1] == "*")
+            {
+                c = a * b;
+                Console.Write("={0}", c);
+            }
+            else if (args[1] == "%")
+            {
+                c = a % b;
+                Console.Write("={0}", c);
+            }
+            else Console.Write("Inputan salah");
+
+
+        }
+
+    }
+}
