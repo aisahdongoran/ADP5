c8570202825c3580a1954e8dcc51b1037c17d041
Nur Aisah Dongoran <42975774+aisahdongoran@users.noreply.github.com>
2019-10-08 13:43:14 +0700

    Add files via upload

---
+++ ConstructorExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Example
+{
+   class Cart
+    {
+        private List<string> items = new List<string>();
+
+        public Cart()
+        {
+            items = new List<string>();
+        }
+        
+        public void AddItems (string item)
+        {
+            items.Add(item);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cart cart = new Cart();
+            cart.AddItems("Ayam");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Example
+{
+    class Computer
+    {
+        private double hardisk;
+        private int baterai;
+        private int lcd;
+        
+        public double Hardisk { get => hardisk; set => hardisk = value; }
+        public int Baterai { get => baterai; set => baterai = value; }
+        public int Lcd { get => lcd; set => lcd = value; }
+        
+        public Computer()
+        {
+            hardisk = 1000.0;
+            baterai = 500;
+            lcd = 150;
+        }
+
+        public Computer(double hardisk, int baterai, int lcd)
+        {
+            this.hardisk = hardisk;
+            this.baterai = baterai;
+            this.lcd = lcd;
+        }
+
+        public int downBrightness()
+        {
+            lcd = lcd - 20;
+            return lcd;
+        }
+        public int upBrightness()
+        {
+            lcd = lcd + 20;
+            return lcd;
+        }
+        public double addData()
+        {
+            hardisk = hardisk - 50;
+            return hardisk;
+        }
+        public double deleteData()
+        {
+            hardisk = hardisk + 50;
+            return hardisk;
+        }
+               
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Computer com = new Computer(200, 80, 400);
+                Console.WriteLine("Hardisk: {0}, Baterai    :{1}, Lcd   :{2}", com.Hardisk, com.Baterai, com.Lcd);
+                com.addData();
+                Console.WriteLine("Hardisk: {0}, Baterai    :{1}, Lcd   :{2}", com.Hardisk, com.Baterai, com.Lcd);
+
+
+            }
+        }
+    }
+}
