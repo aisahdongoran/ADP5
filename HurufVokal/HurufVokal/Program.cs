3d17284461ea94ad67f85c0930d8056370b55026
Nur Aisah Dongoran <42975774+aisahdongoran@users.noreply.github.com>
2019-10-08 13:58:11 +0700

    Add files via upload

---
+++ HurufVokal/HurufVokal/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HurufVokal
+{
+    class Vokal
+    {
+        private string strings;
+
+        public string NamaVokal(string v)
+        {
+            string temp = "";
+            Console.Write("Huruf Vocal    = ");
+            for (int i = 0; i < v.Length; i++)
+            {
+                if (v[i] == 'a' || v[i] == 'e' || v[i] == 'i' || v[i] == 'u' || v[i] == 'o')
+                {
+
+                    Console.Write(v[i] + " ");
+                    temp += v[i];
+                                        
+                }
+            }
+            this.strings = temp;
+            return temp;
+
+        }
+        public int JumlahVokal()
+        {
+            int jumlah = strings.Length;
+            return jumlah;
+        }
+        
+    }
+    class Program
+    {
+        public static int Huruf { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Vokal vokal = new Vokal();
+            Console.WriteLine();
+            Console.Write("Input Kalimat : ");
+            string kalimat = Console.ReadLine();
+            Console.WriteLine(" ");
+            vokal.NamaVokal(kalimat);
+            Console.WriteLine();
+
+            Console.WriteLine("Jumlah:  {0}", vokal.JumlahVokal());
+
+        }
+    }
+}
